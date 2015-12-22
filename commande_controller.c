34c6f8cb39a8d99e6e584cd20f98f24ef17cc524
issam <boutaibism95@gmail.com>
2015-12-22 21:16:27 +0000

    save  commande in a binary file

--- commande_controller.c
+++ commande_controller.c
@@ -12,13 +12,10 @@
 void sauvegarder_commande(const char *nomFichier ,Commande* commande){
     FILE* flot ;
     ///si liste non vide
-    if(commande!=NULL){
-            ///ouvrir un fichier binaire en écriture : suffixe b
-            if((flot=fopen(nomFichier,"wb"))!=NULL){
-                    /// copier la commande dans le fichier passé en argument
-                    fwrite(commande,sizeof(commande),1,flot);
-                /// fermer le fichier
-                fclose(flot);
+    if(commande!=NULL ||((flot=fopen(nomFichier,"wb"))!=NULL)){
+        /// copier la commande dans le fichier passé en argument
+        fwrite(commande,sizeof(commande),1,flot);
+        /// fermer le fichier
+        fclose(flot);
                 }
-     }
 }
